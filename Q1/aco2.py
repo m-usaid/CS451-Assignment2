ba9102e175f8c1d4d9e469f82ca03f00f0a1cdcf
musaid <mr04302@st.habib.edu.pk>
2021-03-21 01:05:18 +0500

    colorings being constructed

--- Q1/aco2.py
+++ Q1/aco2.py
@@ -4,7 +4,7 @@ import random
 import matplotlib.pyplot as plt
 
 class ant:
-    def __init__(self, alpha: int, beta: int) -> None:
+    def __init__(self, alpha = 0.6, beta=0.4) -> None:
         self.graph = None 
         self.coloring = {}
         self.start_vtx = None 
@@ -17,39 +17,84 @@ class ant:
         self.n_colors = 0
 
     def init_sol(self, G:nx.Graph, colorings, startvtx = None):
-        self.avail_colors = sorted(colorings) #.copy()
+        self.avail_colors = sorted(colorings.copy()) #.copy()
         keys = list(G.nodes())
+        keys = convert_int_lst(keys)
         self.assign_colors = {key: None for key in keys}
-        if startvtx == None : self.start_vtx = random.choice(list(G.nodes()))
+        if startvtx == None : 
+            self.start_vtx = random.choice(keys)
         else: 
             self.start_vtx = startvtx
+        # print("yo")
+        # self.start_vtx = startvtx
+        self.visited_vtx = []
+        self.unvisited_vtx = keys.copy()
+        # print(self.unvisited_vtx)
         if (len(self.visited_vtx)==0):
             # assign color to node 
-            pass 
+            # print(self.start_vtx)
+            self.assign_color(self.start_vtx, self.avail_colors[0])
+        return self 
     
     def assign_color(self, vtx, color):
         self.assign_colors[vtx] = color 
         self.visited_vtx.append(vtx)
+        # print(vtx)
         self.unvisited_vtx.remove(vtx)
-    
+
 
     def construct_coloring(self):
-        pass 
+        n_unvisited = len(self.unvisited_vtx)
+        tabu_lst = []
+        for i in range(n_unvisited):
+            nextvtx = self.next_vertex()
+            # print(nextvtx)
+            for j in range(n_nodes):
+                if adj_mat[nextvtx-1, j-1] == 1 :
+                    tabu_lst.append((self.assign_colors[j+1]))
+            for k in self.avail_colors:
+                if (k not in tabu_lst):
+                    self.assign_color(nextvtx, k)
+                    break 
+        self.n_colors = len(set(self.assign_colors.values()))
 
-    def find_dsat(self, startvtx=None):
-        pass
+
+    def find_dsat(self, num_nodes: int, startvtx=None):
+        if startvtx == None: 
+            startvtx = self.start_vtx
+        colored_nbrs = []
+        for i in range(num_nodes):
+            if adj_mat[startvtx-1, i-1] == 1:
+                colored_nbrs.append((self.assign_colors[i+1]))
+        return len(set(colored_nbrs))
 
     def M_ij(self, vtx1, vtx2):
-        pass 
+        return M[vtx1-1, vtx2-1]
 
     def next_vertex(self):
-        pass
-
+        if (len(self.unvisited_vtx) == 0):
+            nextvtx = None 
+        elif len(self.unvisited_vtx) == 1:
+            nextvtx = self.unvisited_vtx[0]
+        else:
+            maxval = 0
+            h_vals = []
+            potential_cands = []
+            available_cands = []
+            for i in self.unvisited_vtx:
+                h_vals.append((self.M_ij(self.start_vtx, i)**self.alpha) * (self.find_dsat(i)**self.beta))
+                potential_cands.append(i)
+            maxval = max(h_vals)
+            for j in range(len(potential_cands)):
+                if h_vals[j] >= maxval:
+                    available_cands.append(potential_cands[j])
+            nextvtx = random.choice(available_cands)
+        self.start_vtx = nextvtx
+        return nextvtx
+   
     def trail(self):
         pass
 
-    def check_equal(self):
-        pass
 
 def read_data(filename: str) -> list:
     listOfEdges = []
@@ -77,15 +122,16 @@ def init_M(G: nx.Graph):
     M = np.ones((n,n), float)
     for vtx in G:
         for nbr in G.neighbors(vtx):
-            M[vtx, nbr] = 0
+            M[int(vtx)-1, int(nbr)-1] = 0
     return M
 
 def create_adj_mat(G: nx.Graph):
     n = G.number_of_nodes()
     adj_mat = np.zeros((n,n), int)
-    for node in list(G.nodes()):
-        for nbr in G.neighbors(node):
-            adj_mat[node, nbr] = 1
+    nodes = list(map(int, G.nodes()))
+    for node in list(nodes):
+        for nbr in G.neighbors(str(node)):
+            adj_mat[node-1, int(nbr)-1] = 1
     return adj_mat
 
 def create_ants(G: nx.Graph, n_ants: int, colors: list):
@@ -101,8 +147,9 @@ def initialize_coloring(G: nx.Graph):
     return colors 
 
 def evap_trail(G: nx.Graph, M, rho: float):
-    for v in G.nodes():
-        for u in G.nodes():
+    nodes = convert_int_lst(list(G.nodes()))
+    for v in nodes():
+        for u in nodes():
             M[v, u] = M[v, u] * (1 - rho)
 
 def select_best(M, ants):
@@ -119,10 +166,40 @@ def select_best(M, ants):
     M = M + elite_matrix
     return elite_ant.best, elite_ant.assign_colors
 
-def solve(G: nx.Graph, n_ants, n_iters, alpha, beta, evap):
-    pass 
+def convert_int_lst(lst: list):
+    lst = list(map(int, lst))
+    return sorted(lst)
+
+def solve(G: nx.Graph, n_ants, n_iters, a, b, evap):
+    global M 
+    global adj_mat
+    global alpha 
+    global beta 
+    global n_nodes 
+    alpha = a
+    beta = b
+    final_sol = {}
+    final_cost = 0
+    n_nodes = G.number_of_nodes()
+    nodes_int = convert_int_lst(list(G.nodes()))
+    adj_mat = create_adj_mat(G)
+    colors = initialize_coloring(G)
+    M = init_M(G)
+    for i in range(n_iters):
+        ants = create_ants(G, n_ants, colors)
+        for ant in ants:
+            ant.construct_coloring()
+            
+            
+        # evap_trail(G, M, evap)
+    
+
+    
 
 graph = create_graph('data/gcol1.txt')
-
+# lst = list(graph.nodes())
+# lst = list(map(int, lst))
+# print(sorted(lst))
 # cols = initialize_coloring(graph)
 # draw_graph(graph)
+solve(graph, 5, 4, 0.6, 0.6, 0.8)
