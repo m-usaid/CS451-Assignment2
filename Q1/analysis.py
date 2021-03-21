a5e6372f44d87a125792077f18ab72a473be9bd2
musaid <mr04302@st.habib.edu.pk>
2021-03-21 21:12:28 +0500

    param_list

--- Q1/analysis.py
+++ Q1/analysis.py
@@ -6,13 +6,17 @@ import numpy as np
 # make analysis function 
 ##  plot data 
 
+param_list = [(0.5, 0.5), (0.5, 0.8), (1, 1.5), (1, 3), (2, 5), (2, 6), (3, 8), (4, 8)]
+
 graph = create_graph('data/gcol1.txt')
 # draw_graph(graph)
 
-# iters = [_ for _ in range(10)]
-# frame = pd.DataFrame(np.array(iters), columns=['iteration'])
-avg, best = solve(graph, 20, 10, 1, 3, 0.5)
-# print(avg)
-d = {"Best Fitness": best, "Average Fitness": avg}
-frame = pd.DataFrame(d)
-print(frame)
+
+
+
+def analyse_data():
+
+    avg, best = solve(graph, 20, 10, 1, 3, 0.5)
+    d = {"Best Fitness": best, "Average Fitness": avg}
+    frame = pd.DataFrame(d)
+    print(frame)
